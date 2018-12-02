a751fc759a7d26993c3d1144a92a009bb8235712
Krissy Dukes <krissydotnet@yahoo.com>
2018-12-02 10:32:51 -0500

    Updates

---
+++ precache-manifest.f44774ec88cab31fd97975f183594a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "c9b714b80a8a9b7ab77f",
+    "url": "/react-gh-pages/static/js/main.c9b714b8.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/react-gh-pages/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "c9b714b80a8a9b7ab77f",
+    "url": "/react-gh-pages/static/css/main.49d21fe3.chunk.css"
+  },
+  {
+    "revision": "da6fcbf5d477218d1cc3e2e427c33eeb",
+    "url": "/react-gh-pages/index.html"
+  }
+];
